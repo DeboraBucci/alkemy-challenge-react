af9887bf43ab44d9b373a533011b23f1caa6b05a
Debora Bucci <dbucci@protonmail.com>
2022-02-03 16:22:08 -0300

    add the <BootstrapNavbar> and the <Meals> components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import BootstrapNavbar from "./BootstrapNavbar";
 import classes from "./Home.module.css";
+import Meals from "./Meals";
 import Search from "./Search";
 
-const Home = (props) => {
+const Home = () => {
   return (
     <main className={classes.home}>
+      <BootstrapNavbar onLogout={onLogout} />
       <Search />
+      <Meals />
     </main>
   );
 };
