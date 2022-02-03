431e3befccb99ab72b356a5aae5d1e81ed643767
Debora Bucci <dbucci@protonmail.com>
2022-02-02 23:41:22 -0300

    delete all the previous code, and add a <main> wrapper with the <Search> component inside

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,12 @@
 import React from "react";
-import { Button, FormControl, InputGroup } from "react-bootstrap";
 import classes from "./Home.module.css";
+import Search from "./Search";
 
-const Home = () => {
+const Home = (props) => {
   return (
-    <section className={classes.home}>
-      <h1>Lily's Cuisine</h1>
-      <p>Your meal in one click</p>
-
-      <InputGroup className={`mb-3 ${classes["input-group"]}`}>
-        <FormControl
-          placeholder="search for meals ..."
-          aria-label="search for meals ..."
-          aria-describedby="basic-addon2"
-        />
-        <Button
-          className={classes.btn}
-          variant="outline-secondary"
-          id="button-addon2"
-        >
-          Search
-        </Button>
-      </InputGroup>
-    </section>
+    <main className={classes.home}>
+      <Search />
+    </main>
   );
 };
 
