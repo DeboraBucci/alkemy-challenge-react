0b3ca7d23092cfcb71ff6556f7ac2531ce92a9de
Debora Bucci <dbucci@protonmail.com>
2022-02-03 16:26:10 -0300

    create a state to check if the user logs in or out. If the user logs in, one function will set a new item in the local storage to allow the user to stay logged in, and will set the state of logged in to true, on the contrary, if the user logs out, the item in the local store will be removed and the state will be set to false

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Home from "./components/Home";
 import LogIn from "./components/login/LogIn";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className="app">
-      <LogIn />
+      {!isLoggedIn && <LogIn onLogin={loginHandler} />}
+      {isLoggedIn && <Home onLogout={logoutHandler} />}
     </div>
   );
 }
