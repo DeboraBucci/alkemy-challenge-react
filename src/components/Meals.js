bf1424bdd66de34305776210f152e468597f9818
Debora Bucci <dbucci@protonmail.com>
2022-02-04 00:44:26 -0300

    add a key to every object inside the 'meals' array, and create a list inside every <Card> component which will display 4 list elements with the calories, health score, rating and cooking time of each meal

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -12,6 +12,16 @@ const Meals = () => {
       tag: "",
       time: "",
       healthPoints: "",
+      key: "meal1",
+    },
+    {
+      img: Meal1,
+      title: "Mozzarella Pizza with Tomatoe",
+      text: "this pizza has mozzarella, tomatoe, garlic...",
+      tags: "",
+      time: "",
+      healthPoints: "",
+      key: "meal2",
     },
     {
       img: Meal1,
@@ -20,6 +30,7 @@ const Meals = () => {
       tag: "",
       time: "",
       healthPoints: "",
+      key: "meal3",
     },
     {
       img: Meal1,
@@ -28,14 +39,7 @@ const Meals = () => {
       tag: "",
       time: "",
       healthPoints: "",
-    },
-    {
-      img: Meal1,
-      title: "Mozzarella Pizza with Tomatoe",
-      text: "this pizza has mozzarella, tomatoe, garlic...",
-      tag: "",
-      time: "",
-      healthPoints: "",
+      key: "meal4",
     },
   ];
 
@@ -52,6 +56,34 @@ const Meals = () => {
             <Card.Body className={classes["card-body"]}>
               <Card.Title>{meal.title}</Card.Title>
               <Card.Text>{meal.text}</Card.Text>
+
+              <ul className={classes.list}>
+                <li>
+                  <i className={`fas fa-fire-alt ${classes.fire}`}></i>{" "}
+                  <p>
+                    <span className={classes.strong}>2,200</span> calories
+                  </p>
+                </li>
+                <li>
+                  <i className={`fas fa-heartbeat ${classes.heart}`}></i>
+                  <p>
+                    Health Score <span className={classes.strong}>67</span>
+                  </p>
+                </li>
+                <li>
+                  <i className={`far fa-star ${classes.rate}`}></i>{" "}
+                  <p>
+                    <span className={classes.strong}>4.9</span> rating (482)
+                  </p>
+                </li>
+                <li>
+                  <i className={`far fa-clock ${classes.time}`}></i>{" "}
+                  <p>
+                    Cooking time <span className={classes.strong}>39 mins</span>
+                  </p>
+                </li>
+              </ul>
+
               <Button className={classes.btn} variant="primary">
                 Add to Cart <i className={`fas fa-plus ${classes.icon}`}></i>
               </Button>
