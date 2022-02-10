2fcd8e6fb3d7b4db1907afe7af2acac82fc52124
Debora Bucci <dbucci@protonmail.com>
2022-02-09 23:07:41 -0300

    add a wrapping <form> to the search bar with the event listener on submit, add an on change event listener to the <input> and a type of 'submit' to the button. Then, create two states to store the entered input and the meals from the api. Also, create two functions, one to store the entered input in the state, and the other to request meals through a get call to the api if the entered input is at least two characters long

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, FormControl, InputGroup } from "react-bootstrap";
 import classes from "./Search.module.css";
+import Axios from "axios";
 
 const Search = (props) => {
+  const [enteredInput, setEnteredInput] = useState();
+  const [meals, setMeals] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (enteredInput.trim().length < 2) return;
+
+    Axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${enteredInput.trim()}&apiKey=602e462c798141faa4b7fd415ba7a619`
+    ).then(function (response) {
+      const mealsData = response.data.results;
+
+      mealsData.map((meal) =>
+        setMeals((prevMeals) => [
+          ...prevMeals,
+          {
+            id: meal.id,
+            title: meal.title,
+            image: meal.image,
+          },
+        ])
+      );
+    });
+  };
+
+  const inputHandler = (e) => {
+    console.log(e.target.value);
+    setEnteredInput(e.target.value);
+  };
+
   return (
     <section className={classes.search}>
       <h1>Lily's Cuisine</h1>
       <p>Your meal in one click</p>
 
-      <InputGroup className={`mb-3 ${classes["input-group"]}`}>
-        <FormControl
-          placeholder="search for meals ..."
-          aria-label="search for meals ..."
-          aria-describedby="basic-addon2"
-        />
-        <Button
-          className={classes.btn}
-          variant="outline-secondary"
-          id="button-addon2"
-        >
-          Search
-        </Button>
-      </InputGroup>
+      <form onSubmit={submitHandler}>
+        <InputGroup className={`mb-3 ${classes["input-group"]}`}>
+          <FormControl
+            onChange={inputHandler}
+            placeholder="search for meals ..."
+            aria-label="search for meals ..."
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            type="submit"
+            className={classes.btn}
+            variant="outline-secondary"
+            id="button-addon2"
+          >
+            Search
+          </Button>
+        </InputGroup>
+      </form>
     </section>
   );
 };
