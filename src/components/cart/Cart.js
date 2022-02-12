bd613bae1ce63ac48ae0310f649925000b1b113f
Debora Bucci <dbucci@protonmail.com>
2022-02-12 08:26:58 -0300

    add some icons from Font Awesome, and change the previous table for a unordered list, and use the 'CartContext' to map over the items in the cart to display the <CartItem> component in each iteration, and add some clases and styles

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,41 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faXmark,
+  faUtensils,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
+import { faClock, faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import CartContext from "../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  console.log(cartCtx);
+
+  const cartItems = (
+    <ul className={classes.list}>
+      {cartCtx.totalDishes.map((dish) => (
+        <CartItem
+          key={dish.item.id}
+          title={dish.item.title}
+          image={dish.item.image}
+          price={dish.item.price}
+        />
+      ))}
+    </ul>
+  );
+
   return (
     <React.Fragment>
       <div className={classes.backdrop}></div>
       <div className={classes.cart}>
-        <h3>My Cart</h3>
+        <div className={classes["title-box"]}>
+          <h3>My Cart</h3>
+        </div>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Qty</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Vanilla</td>
-              <td>2</td>
-              <td>$50</td>
-            </tr>
-          </tbody>
-        </table>
+        {cartItems}
 
         <div>
           <div className={classes.info}>
-            <p>Number of Dishes: 4</p>
-            <p>Preparation Time: 2hs</p>
-            <p>Total Amount: $350</p>
+            <p>
+              <FontAwesomeIcon icon={faUtensils} /> 4
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faClock} /> 2hs
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faDollarSign} /> 350
+            </p>
           </div>
           <div className={classes.actions}>
             <button className={classes["btn-close"]}>Close</button>
-            <button className={classes["btn-order"]}>Order</button>
+            <button className={classes["btn-order"]}>
+              Order <FontAwesomeIcon icon={faCircleCheck} />
+            </button>
           </div>
           <button onClick={props.onCloseCart} className={classes["btn-x"]}>
             <FontAwesomeIcon icon={faXmark} />
