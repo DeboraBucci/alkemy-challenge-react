f9f799a49978af3dabc0aa614c05e32df36ccbdb
Debora Bucci <dbucci@protonmail.com>
2022-02-12 05:22:42 -0300

    add to the inside of the cart a table, two buttons, and a title

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -8,10 +8,39 @@ const Cart = (props) => {
     <React.Fragment>
       <div className={classes.backdrop}></div>
       <div className={classes.cart}>
-        <button onClick={props.onCloseCart} className={classes["btn-close"]}>
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-        <div>Cart</div>
+        <h3>My Cart</h3>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Qty</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Vanilla</td>
+              <td>2</td>
+              <td>$50</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div>
+          <div className={classes.info}>
+            <p>Number of Dishes: 4</p>
+            <p>Preparation Time: 2hs</p>
+            <p>Total Amount: $350</p>
+          </div>
+          <div className={classes.actions}>
+            <button className={classes["btn-close"]}>Close</button>
+            <button className={classes["btn-order"]}>Order</button>
+          </div>
+          <button onClick={props.onCloseCart} className={classes["btn-x"]}>
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
