3d5934ca5812b25104283aabb1f3c96dae1253f5
Debora Bucci <dbucci@protonmail.com>
2022-02-13 20:26:07 -0300

    add dynamic values to the paragraphs

--- src/components/bar/Bar.js
+++ src/components/bar/Bar.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Bar.module.css";
+import CartContext from "../store/cart-context";
 
 const Bar = () => {
+  const cartCtx = useContext(CartContext);
+  const hours = Math.floor(+cartCtx.totalTime / 60);
+  const minutes = Math.floor(+cartCtx.totalTime % 60);
+
+  console.log(hours, minutes);
+
   return (
     <div className={classes.bar}>
-      <p>Total: $160</p>
-      <p>Preparation Time: 1:30hs</p>
-      <p>Health Score: 85.4</p>
-      <p>Dishes: 3</p>
+      <p>Total: ${Math.round(cartCtx.totalPrice)}</p>
+      <p>
+        Preparation Time: {hours ? `${hours} h` : ""}{" "}
+        {minutes ? `${minutes} min` : ""}
+      </p>
+      <p>
+        Health Score: {cartCtx.totalHealthScore / cartCtx.totalDishes.length}
+      </p>
+      <p>Dishes: {cartCtx.totalDishes.length}</p>
       <button>Order</button>
     </div>
   );
