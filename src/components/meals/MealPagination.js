8e436d7f02a061c6c37d856a1f55cc5ed63ba9a0
Debora Bucci <dbucci@protonmail.com>
2022-02-14 13:26:36 -0300

    add new buttons to the pagination to be able to jump to the first and last pagination, and the next and previous one. Also, style pagination a bit further

--- src/components/meals/MealPagination.js
+++ src/components/meals/MealPagination.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { Pagination } from "react-bootstrap";
 import MealItem from "./MealItem";
 import classes from "./MealPagination.module.css";
+import {
+  faAngleRight,
+  faAngleLeft,
+  faAnglesRight,
+  faAnglesLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MealPagination = ({ displayedMeals }) => {
   let items = [];
@@ -9,7 +16,7 @@ const MealPagination = ({ displayedMeals }) => {
 
   const [active, setActive] = useState(1);
 
-  for (let number = 1; number < displayedMeals.length; number++) {
+  for (let number = 1; number <= displayedMeals.length; number++) {
     items.push(
       <Pagination.Item key={number} active={number === active}>
         {number}
@@ -23,6 +30,30 @@ const MealPagination = ({ displayedMeals }) => {
     }
 
     setActive(Number(e.target.text));
+  };
+
+  const nextPaginationHandler = () => {
+    setActive((prevNum) => {
+      if (prevNum === displayedMeals.length) return prevNum;
+      const newNum = prevNum + 1;
+      return newNum;
+    });
+  };
+
+  const prevPaginationHandler = () => {
+    setActive((prevNum) => {
+      if (prevNum === 1) return prevNum;
+      const newNum = prevNum - 1;
+      return newNum;
+    });
+  };
+
+  const firstPaginationHandler = () => {
+    setActive(1);
+  };
+
+  const lastPaginationHandler = () => {
+    setActive(displayedMeals.length);
   };
 
   return (
@@ -39,10 +70,28 @@ const MealPagination = ({ displayedMeals }) => {
                   ))}
                 </div>
                 <Pagination
-                  className={classes["pag-num"]}
                   onClick={paginationHandler}
+                  className={classes["pag-num"]}
                 >
+                  <button
+                    className={classes.first}
+                    onClick={firstPaginationHandler}
+                  >
+                    <FontAwesomeIcon icon={faAnglesLeft} />
+                  </button>
+                  <button onClick={prevPaginationHandler}>
+                    <FontAwesomeIcon icon={faAngleLeft} />
+                  </button>
                   {items}
+                  <button onClick={nextPaginationHandler}>
+                    <FontAwesomeIcon icon={faAngleRight} />
+                  </button>
+                  <button
+                    className={classes.last}
+                    onClick={lastPaginationHandler}
+                  >
+                    <FontAwesomeIcon icon={faAnglesRight} />
+                  </button>
                 </Pagination>
               </div>
             )}
