fea635d3c73738361c7f75877a64c79d1e7dad61
Debora Bucci <dbucci@protonmail.com>
2022-02-14 08:44:58 -0300

    create a chunk variable and set it to the amount of meals you want displayed in each pagination, then use a for loop to push those five meals into one array, and then add all the arrays to 'displayedMeals'. Also, delete unnecessary wrapper <div> and add the <MealPagination> component

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -1,34 +1,37 @@
 import React, { useContext, useEffect, useState } from "react";
 import classes from "./Meals.module.css";
 import { Spinner } from "react-bootstrap";
-import MealItem from "./MealItem";
+import MealPagination from "./MealPagination";
 
 const Meals = ({ meals, waiting }) => {
   const [displayedMeals, setDisplayedMeals] = useState([]);
+
   useEffect(() => {
     if (waiting) {
       setDisplayedMeals([]);
     } else {
-      setDisplayedMeals(meals);
+      let chunk = 5;
+      const arr = [];
+
+      for (let i = 0; i < meals.length; i += chunk) {
+        arr.push(meals.slice(i, i + chunk));
+      }
+      setDisplayedMeals(arr);
     }
   }, [waiting, meals]);
 
   return (
     <section className={classes.meals}>
-      <div className={classes["meals-box"]}>
-        {waiting && displayedMeals.length === 0 && (
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        )}
-        {!waiting && displayedMeals.length === 0 ? (
-          <p>No meals found.</p>
-        ) : (
-          displayedMeals.map((meal) => (
-            <MealItem key={Math.random()} meal={meal} />
-          ))
-        )}
-      </div>
+      {waiting && displayedMeals.length === 0 && (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      {!waiting && displayedMeals.length === 0 ? (
+        <p>No meals found.</p>
+      ) : (
+        <MealPagination displayedMeals={displayedMeals} />
+      )}
     </section>
   );
 };
