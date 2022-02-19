7ff2a90a40cd3d13fe6af9514f2b152db8bfc381
Debora Bucci <dbucci@protonmail.com>
2022-02-19 16:40:46 -0300

    change the button's icon, and let the component receive an additional class via props to apply alongside the other classes

--- src/components/UI/MoreInfoButton.js
+++ src/components/UI/MoreInfoButton.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./MoreInfoButton.module.css";
 
 import { mealInfoHandler } from "../../Data";
 
-const MoreInfoButton = ({ meal, setInfoHandler }) => {
+const MoreInfoButton = ({ meal, setInfoHandler, className }) => {
   const moreInfoHandler = () => {
     const mealInfo = mealInfoHandler(meal);
     setInfoHandler(mealInfo);
@@ -17,10 +17,10 @@ const MoreInfoButton = ({ meal, setInfoHandler }) => {
   return (
     <Button
       onClick={moreInfoHandler}
-      className={classes["btn-info"]}
+      className={`${classes["btn-info"]} ${className}`}
       variant="primary"
     >
-      More Info <FontAwesomeIcon className={classes.icon} icon={faPlus} />
+      More Info <FontAwesomeIcon className={classes.icon} icon={faCircleInfo} />
     </Button>
   );
 };
