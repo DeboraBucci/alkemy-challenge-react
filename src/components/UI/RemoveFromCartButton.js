bad2989435c43690e56bdd581e079b508ac73214
Debora Bucci <dbucci@protonmail.com>
2022-02-19 16:36:20 -0300

    make the component accept and apply a passed class in addition to the other classes

--- src/components/UI/RemoveFromCartButton.js
+++ src/components/UI/RemoveFromCartButton.js
@@ -5,9 +5,12 @@ import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 import classes from "./RemoveFromCartButton.module.css";
 
-const RemoveFromCartButton = ({ onClick }) => {
+const RemoveFromCartButton = ({ onClick, className }) => {
   return (
-    <button onClick={onClick} className={classes["btn-delete"]}>
+    <button
+      onClick={onClick}
+      className={`${classes["btn-delete"]} ${className}`}
+    >
       <FontAwesomeIcon icon={faTrashCan} />
     </button>
   );
