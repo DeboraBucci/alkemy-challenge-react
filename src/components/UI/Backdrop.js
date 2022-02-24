c15e936c7390c5bb0e70889deadfc35d820f9933
Debora Bucci <dbucci@protonmail.com>
2022-02-24 13:50:58 -0300

    create a new component called 'Backdrop'

---
+++ src/components/UI/Backdrop.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Backdrop.module.css";
+
+const Backdrop = ({ onClick }) => {
+  return <div onClick={onClick} className={classes.backdrop}></div>;
+};
+
+export default Backdrop;
