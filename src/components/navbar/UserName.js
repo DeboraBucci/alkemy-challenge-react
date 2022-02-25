46cadb020f2f4b308dcaa1475ec0aad9d615767f
Debora Bucci <dbucci@protonmail.com>
2022-02-25 15:43:35 -0300

    create a new component called 'UserName' to display a dynamic greeting to the user. The user can change the name at any time by clicking the displayed button

---
+++ src/components/navbar/UserName.js
@@ -0,0 +1,50 @@
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import classes from "./UserName.module.css";
+
+const UserName = () => {
+  const [isNameChanging, setIsNameChanging] = useState(false);
+  const [name, setName] = useState("user");
+
+  const changeNameHandler = () => {
+    setIsNameChanging(true);
+  };
+
+  const setNameHandler = (e) => {
+    const enteredText = e.target.value;
+
+    if (e.code === "Enter" || e._reactName === "onBlur") {
+      !(enteredText === "") && setName(enteredText);
+
+      setIsNameChanging(false);
+    }
+  };
+
+  const changeNameInput = (
+    <input
+      placeholder="your name"
+      className={classes["change-name-input"]}
+      onKeyPress={setNameHandler}
+      onBlur={setNameHandler}
+    />
+  );
+
+  const changeNameButton = (
+    <button onClick={changeNameHandler}>
+      <FontAwesomeIcon icon={faPenToSquare} />
+    </button>
+  );
+
+  const dynamicParagraphContent = !isNameChanging ? name : changeNameInput;
+
+  return (
+    <div className={classes.greeting}>
+      <p>Hello, {dynamicParagraphContent}!</p>
+
+      {!isNameChanging && changeNameButton}
+    </div>
+  );
+};
+
+export default UserName;
