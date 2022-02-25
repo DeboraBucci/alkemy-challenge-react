2f3046dc964d24e72804b40e79673cd0019ca05b
Debora Bucci <dbucci@protonmail.com>
2022-02-25 10:46:53 -0300

    remove the previous fallback div for when there is no image in the card, and use the 'EmptyMealCard' instead. Also, return the entire content when there is a meal, and if not, return only the <EmptyMealCard>. Finally, clean and organize the code

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -2,30 +2,27 @@ import React from "react";
 
 import classes from "./CartItem.module.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import RemoveFromCartButton from "../UI/RemoveFromCartButton";
+import EmptyMealCard from "../UI/EmptyMealCard";
 
 const CartItem = (props) => {
-  return (
-    <li className={`${classes["cart-item"]} ${props.className}`}>
+  const content = props.image ? (
+    <>
       <div className={classes["img-box"]}>
-        {props.image && <img width={200} alt={props.title} src={props.image} />}
-        {!props.image && (
-          <div className={classes.fallback}>
-            <div className={classes.circle}>
-              <FontAwesomeIcon icon={faUtensils} />
-            </div>
-          </div>
-        )}
+        <img width={200} alt={props.title} src={props.image} />
       </div>
       <div className={classes.text}>
         <h3>{props.title}</h3>
         {props.price && <p className={classes.price}>Price: $ {props.price}</p>}
-        {!props.price && <p>Add a meal!</p>}
       </div>
       {props.title && <RemoveFromCartButton onClick={props.onRemove} />}
-    </li>
+    </>
+  ) : (
+    <EmptyMealCard />
+  );
+
+  return (
+    <li className={`${classes["cart-item"]} ${props.className}`}>{content}</li>
   );
 };
 
