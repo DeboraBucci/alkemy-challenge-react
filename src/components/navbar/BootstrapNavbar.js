9c240d31c85391111c0fda2f0130d46a572e2854
Debora Bucci <dbucci@protonmail.com>
2022-02-25 15:12:11 -0300

    remove the dropdown and just add another link for loggin out

--- src/components/navbar/BootstrapNavbar.js
+++ src/components/navbar/BootstrapNavbar.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import brand from "../../imgs/brand.webp";
 import classes from "./BootstrapNavbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faSignOut } from "@fortawesome/free-solid-svg-icons";
 import CartContext from "../store/cart-context";
 
 const BootstrapNavbar = ({ onLogout, onOpenCart }) => {
@@ -41,11 +41,10 @@ const BootstrapNavbar = ({ onLogout, onOpenCart }) => {
               {cartCtx.totalDishes.length} items{" "}
               <FontAwesomeIcon icon={faCartShopping} />
             </Nav.Link>
-            <NavDropdown title="User" id="collasible-nav-dropdown">
-              <NavDropdown.Item onClick={onLogout}>
-                <i className="fas fa-sign-out-alt"></i> Log Out
-              </NavDropdown.Item>
-            </NavDropdown>
+
+            <Nav.Link className={classes.logout} onClick={onLogout}>
+              Log Out <FontAwesomeIcon icon={faSignOut} />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
