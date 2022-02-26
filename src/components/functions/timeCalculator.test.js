9a090775f0b373df032b170089f62877ac460fbf
Debora Bucci <dbucci@protonmail.com>
2022-02-26 10:06:59 -0300

    generate more tests to check if the functions return what is expected if the user passes only zeros to the functions

--- src/components/functions/timeCalculator.test.js
+++ src/components/functions/timeCalculator.test.js
@@ -3,4 +3,7 @@ const { timeCalculator } = require("./timeCalculator");
 test("should return array with hours and minutes", () => {
   const time = timeCalculator(90);
   expect(time).toStrictEqual([1, 30]);
+
+  const time2 = timeCalculator(0);
+  expect(time2).toStrictEqual([0, 0]);
 });
--- src/components/functions/timeTextGenerator.test.js
+++ src/components/functions/timeTextGenerator.test.js
@@ -3,4 +3,7 @@ const { timeTextGenerator } = require("./timeTextGenerator");
 test("should return array with two strings, one for the hours, and the other with the minutes", () => {
   const time = timeTextGenerator(1, 30);
   expect(time).toStrictEqual(["1 h", "30 min"]);
+
+  const time2 = timeTextGenerator(0, 0);
+  expect(time2).toStrictEqual(["", "0 min"]);
 });
