871b2e0efedc65bcef82956d9bf379d8d15231c8
Debora Bucci <dbucci@protonmail.com>
2022-02-26 10:05:40 -0300

    make the function return an array with an empty string and '0 min', if the hours and minutes passed as arguments are zero

--- src/components/functions/timeTextGenerator.js
+++ src/components/functions/timeTextGenerator.js
@@ -1,4 +1,6 @@
 exports.timeTextGenerator = (hs, mins) => {
+  if (hs === 0 && mins === 0) return ["", "0 min"];
+
   const hours = hs !== 0 ? `${hs} h` : "";
   const minutes = mins !== 0 ? `${mins} min` : "";
 
