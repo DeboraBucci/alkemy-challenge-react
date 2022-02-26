9b7eb38e5bbccea3e6ceecc44b1138b348770452
Debora Bucci <dbucci@protonmail.com>
2022-02-26 14:16:05 -0300

    change class' name from 'cart' to 'modal'

--- src/components/UI/ModalComp.js
+++ src/components/UI/ModalComp.js
@@ -3,7 +3,7 @@ import React from "react";
 import classes from "./ModalComp.module.css";
 
 const ModalComp = ({ className, children }) => {
-  return <div className={`${classes.cart} ${className}`}>{children}</div>;
+  return <div className={`${classes.modal} ${className}`}>{children}</div>;
 };
 
 export default ModalComp;
