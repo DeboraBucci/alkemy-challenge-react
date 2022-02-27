a9b8ce19356f5b265ab4aa8b401fea3b78ff70d6
Debora Bucci <dbucci@protonmail.com>
2022-02-27 10:44:20 -0300

    create a new component called 'SearchHeader' which displays the title and slogan of the page (this code was taken from the 'Search' component and slightly modified using the 'randomNumGenerator' function)

---
+++ src/components/home/search/SearchHeader.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import classes from "./SearchHeader.module.css";
+
+const { randomNumGenerator } = require("../../functions/randomNumGenerator");
+
+const SearchHeader = () => {
+  const [slogan, setSlogan] = useState();
+
+  useEffect(() => {
+    const randomNum = randomNumGenerator(1, 3);
+    randomNum === 1 && setSlogan("Your meal in one click!");
+    randomNum === 2 && setSlogan("We cook, you enjoy!");
+    randomNum === 3 && setSlogan("Your meal, one click away!");
+  }, []);
+
+  return (
+    <header className={classes.brand}>
+      <h1>Lily's Cuisine</h1>
+      <p>{slogan}</p>
+    </header>
+  );
+};
+
+export default SearchHeader;
