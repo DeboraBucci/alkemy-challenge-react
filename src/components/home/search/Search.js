dfff56665e548d758bb6fbe732051cec29847f1d
Debora Bucci <dbucci@protonmail.com>
2022-02-27 11:35:26 -0300

    use the new function to receive the meals array and pass it directly to the 'onSearchMeals' function, then delete the 'useEffect' hook and all the unnecessary code

--- src/components/home/search/Search.js
+++ src/components/home/search/Search.js
@@ -1,80 +1,33 @@
-import React, { useEffect } from "react";
-import classes from "./Search.module.css";
-import Axios from "axios";
-import SweetAlert from "../../UI/SweetAlert";
+import React from "react";
+
 import FormikForm from "./FormikForm";
 import SearchHeader from "./SearchHeader";
+
 import { mealSearchLinkModifier } from "../../../Data";
-import getMealsData from "../../functions/getMealsData";
+import {
+  getMealsData,
+  setMealsData,
+} from "../../functions/mealsDataManagement";
 
-const { randomNumGenerator } = require("../../functions/randomNumGenerator");
+import classes from "./Search.module.css";
 
-const Search = ({
-  onSearchMeals,
-  meals,
-  setIsWaiting,
-  waiting,
-  setIsMealsShown,
-  setMeals,
-}) => {
-  useEffect(() => {
-    onSearchMeals(meals);
-  }, [meals]);
-
+const Search = ({ onSearchMeals, setIsWaiting, waiting, setIsMealsShown }) => {
   const onSubmit = async (text, preferences) => {
     setIsWaiting(true);
     setIsMealsShown(true);
 
     const link = mealSearchLinkModifier(text, preferences);
+    const data = await getMealsData(link);
+    const meals = await setMealsData(data);
 
-    const mealsArr = [];
-
-    try {
-      const data = await getMealsData(link);
-
-      await data.forEach(async (meal) => {
-        const rating = randomNumGenerator(3, 5).toFixed(2);
-        const raters = Math.floor(randomNumGenerator(2, 3000));
-
-        const calories = `${Math.round(meal.nutrition.nutrients[0].amount)} ${
-          meal.nutrition.nutrients[0].unit
-        }`;
-
-        mealsArr.push({
-          id: meal.id,
-          title: meal.title,
-          image: meal.image,
-          calories: calories,
-          healthScore: meal.healthScore,
-          diets: meal.diets,
-          price: meal.pricePerServing,
-          time: meal.readyInMinutes,
-          servings: meal.servings,
-          ingredients: meal.nutrition.ingredients,
-          caloricBreakdown: meal.nutrition.caloricBreakdown,
-          summary: meal.summary,
-          nutrients: meal.nutrition.nutrients,
-          isVegan: meal.vegan,
-          rating: rating,
-          raters: raters,
-        });
-      });
-
-      await setMeals(mealsArr);
-    } catch (err) {
-      setMeals([]);
-
-      SweetAlert({
-        title: err.name,
-        text: err.message,
-      });
-    }
+    await onSearchMeals(meals);
   };
 
   return (
     <section id="search" className={classes.search}>
       <SearchHeader />
       <FormikForm onSubmit={onSubmit} waiting={waiting} />
+
       <p>You have to choose two vegan and two non vegan meals for your menu.</p>
     </section>
   );
